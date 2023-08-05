311c5b653a6d7bc1f2023a6afeae57317f4efcd1
whitespacerespecter <140878425+whitespacerespecter@users.noreply.github.com>
2023-08-05 10:28:05 +0200

    uploaded project files

---
+++ index.js
@@ -0,0 +1,41 @@
+let homeScore = document.getElementById("homeScore")
+let guestScore = document.getElementById("guestScore")
+
+console.log(homeScore)
+console.log(guestScore)
+
+let homeCount = 0;
+let guestCount = 0;
+
+function home1() {
+    homeCount += 1
+    homeScore.textContent = homeCount 
+}
+function home2() {
+    homeCount += 2
+    homeScore.textContent = homeCount 
+}
+function home3() {
+    homeCount += 3
+    homeScore.textContent = homeCount 
+}
+
+function guest1() {
+    guestCount += 1
+    guestScore.textContent = guestCount 
+}
+function guest2() {
+    guestCount += 2
+    guestScore.textContent = guestCount 
+}
+function guest3() {
+    guestCount += 3
+    guestScore.textContent = guestCount 
+}
+
+function reset() {
+    homeCount.textContent = 0;
+    homeScore.textContent = 0;
+    guestCount = 0;
+    guestScore = 0;
+}
